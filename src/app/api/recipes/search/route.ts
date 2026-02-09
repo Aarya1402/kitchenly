06901a397885c61a08e474bdca5742b5183de9ce
Aarya Patel <aarya.patel@drcsystems.com>
2026-02-09 12:24:44 +0530

    fix: deployment

--- src/app/api/recipes/search/route.ts
+++ src/app/api/recipes/search/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
-import { prisma } from "@/lib/db";
-import { Prisma } from "@/generated/prisma";
+// import { prisma } from "@/lib/db";
+// import { Prisma } from "@/generated/prisma";
 
 export async function GET(req: Request) {
   // try {
